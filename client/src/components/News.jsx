c1de6f1d42cab52d4277ef203477846ec1917157
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-08 23:58:30 +0100

    refactor: Update News component layout and add news cards

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -48,6 +48,43 @@ export default function News() {
     </div>
     </div>
 
+
+<div className="flex justify-center items-center space-x-10 px-10 mt-10">
+
+<div className="relative card card-side bg-base-100 shadow-lg h-72 rounded-none">
+    <figure className="w-1/3">
+        <img src="./news3.jpg" alt="air" className="w-full h-full object-cover" />
+    </figure>
+    <div className="card-body w-2/3 bg-gray-100 group hover:bg-[#00C0E4] hover:text-white">
+    <h2 className="card-title font-medium">POST & PARCEL</h2>
+    <h2 className="card-title text-sm font-medium">Admin <ChatBubbleLeftRightIcon className="h-6 w-6 text-[#00C0E4] group-hover:text-white"/> 40 </h2>
+    <p className="text-lg text-gray-500 group-hover:text-white">
+    DHL Metro enters strategic partnership with Easyship to provide enhanced, affordable shipping solutions to SMB’s...
+    </p>
+    <div className='absolute top-0 left-0 flex flex-col bg-black text-white h-14 w-12 text-center group-hover:bg-[#00C0E4] group-hover:text-white'>
+        <p>28</p>
+        <p>May</p>
+    </div>
+    </div>
+</div>
+
+<div className="relative card card-side bg-base-100 shadow-lg h-72 rounded-none">
+    <figure className="w-1/3">
+        <img src="./news4.jpg" alt="air" className="w-full h-full object-cover" />
+    </figure>
+    <div className="card-body w-2/3 bg-gray-100 group hover:bg-[#00C0E4] hover:text-white">
+    <h2 className="card-title font-medium">SUPPLY CHAIN DIVE</h2>
+    <h2 className="card-title text-sm font-medium">Admin <ChatBubbleLeftRightIcon className="h-6 w-6 text-[#00C0E4] group-hover:text-white"/> 15 </h2>
+    <p className="text-lg text-gray-500 group-hover:text-white">
+    Easyship, a Stripe for global e-commerce shipping, raises $4M. Its service adds shipment options to e-commerce pages to make it simple for retailers to offer overseas shipping, and customers to receive product in any market...
+    </p>
+    <div className='absolute top-0 left-0 flex flex-col bg-black text-white h-14 w-12 text-center group-hover:bg-[#00C0E4] group-hover:text-white'>
+        <p>21</p>
+        <p>May</p>
+    </div>
+    </div>
+</div>
+
 </div>
     </>
   )
