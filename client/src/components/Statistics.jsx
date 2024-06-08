cd9c7470118b5f1c7cc5bb62bd4f22206eb2e6a3
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-09 00:31:09 +0100

    refactor: Update Statistics component layout and styling

---
+++ client/src/components/Statistics.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Statistics() {
+  return (
+    <>
+<div className="flex justify-center items-center space-x-44 text-center mt-20">
+  <div className="relative">
+    <p className="text-7xl font-bold text-gray-200">100,000</p>
+    <div className="flex justify-center items-center">
+    <div>   
+    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">100,000+</p>
+    </div> 
+    <div>   
+    <p className="absolute top-2 left-14 text-xl font-semibold text-[#01205F]">Trusted by Businesses </p>
+    </div> 
+    </div>
+  </div>
+
+  <div className="relative">
+    <p className="text-7xl font-bold text-gray-200">157</p>
+    <div className="flex justify-center items-center">
+    <div>   
+    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">157</p>
+    </div> 
+    <div>   
+    <p className="absolute top-2 left-14 text-xl font-semibold text-[#01205F]">Countries Covered</p>
+    </div> 
+    </div>
+  </div>
+
+  <div className="relative">
+    <p className="text-7xl font-bold text-gray-200">980</p>
+    <div className="flex justify-center items-center">
+    <div>   
+    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">980</p>
+    </div> 
+    <div>   
+    <p className="absolute top-2 left-14 text-xl font-semibold text-[#01205F]">Tons of Goods</p>
+    </div> 
+    </div>
+  </div>
+
+  <div className="relative">
+    <p className="text-7xl font-bold text-gray-200">100,000</p>
+    <div className="flex justify-center items-center">
+    <div>   
+    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">100,000+=</p>
+    </div> 
+    <div>   
+    <p className="absolute top-2 left-14 text-xl font-semibold text-[#01205F]">Satisfied Clients</p>
+    </div> 
+    </div>
+  </div>
+
+</div>
+    </>
+  )
+}
