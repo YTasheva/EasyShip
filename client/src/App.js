be2dacb04110980e6b2bdcf0958989cd30e729e3
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-08 22:51:03 +0100

    refactor: Update App.js to include all necessary components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Slider from './components/Slide';
+import Welcome from './components/Welcome';
+import Service from './components/Service';
+import Stats from './components/Statistics';
+import Testimonials from './components/Testimonials';
+import Clients from './components/Clients';
+import News from './components/News';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar/>
+   <Slide/>
+   <Welcome/>
+   <Service/>
+   <Statistics/>
+   <Testimonials/>
+   <Clients/>
+   <News/>
+   <Footer/>
+   </>
   );
 }
 
