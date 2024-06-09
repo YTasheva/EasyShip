e5a928871b6fc245b7ffa0809d8f501b060a4e89
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-09 01:43:54 +0100

    refactor: Update Statistics component to display accurate client count

--- client/src/components/Statistics.jsx
+++ client/src/components/Statistics.jsx
@@ -41,10 +41,10 @@ export default function Statistics() {
   </div>
 
   <div className="relative">
-    <p className="text-7xl font-bold text-gray-200">100,000</p>
+    <p className="text-7xl font-bold text-gray-200">15,350</p>
     <div className="flex justify-center items-center">
     <div>   
-    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">100,000+=</p>
+    <p className="absolute top-5 left-2 text-2xl font-semibold text-[#01205F]">15,350+=</p>
     </div> 
     <div>   
     <p className="absolute top-2 left-14 text-xl font-semibold text-[#01205F]">Satisfied Clients</p>
