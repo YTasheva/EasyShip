d535d271b3627534648d660a519783de7adba413
andrewyu209@gmail.com <andrewyu209@gmail.com>
2024-06-09 15:34:32 -0400

    Extracted Data

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -4,21 +4,6 @@ from .azure import analyze_document
 from django.core.files.storage import FileSystemStorage
 import traceback
 
-def analyze_result_to_dict(analyze_result):
-    # Converts the AnalyzeResult object to a dictionary with only relevant fields
-    def field_to_dict(field):
-        if isinstance(field, list):
-            return [field_to_dict(f) for f in field]
-        elif isinstance(field, dict):
-            return {k: field_to_dict(v) for k, v in field.items()}
-        elif hasattr(field, "__dict__"):
-            # Extract only the 'content' and 'value' fields if they exist
-            field_dict = {k: field_to_dict(v) for k, v in field.__dict__.items() if k in ['content', 'value']}
-            return field_dict
-        else:
-            return field
-
-    return field_to_dict(analyze_result)
 
 @api_view(['POST'])
 def analyze_document_view(request):
@@ -31,9 +16,10 @@ def analyze_document_view(request):
         try:
             print(f"Analyzing document: {file_path}")
             result = analyze_document(file_path)
-            result_dict = analyze_result_to_dict(result)
-            print(f"Analysis result: {result_dict}")
-            return JsonResponse(result_dict, safe=False)  # Set safe to False
+            for document in result.documents: 
+                for field in document.fields.items():
+                    field_name, field_value = field
+                    print(field_name, " value: ", field_value.value)
         except Exception as e:
             print(f"Error analyzing document: {traceback.format_exc()}")
             return JsonResponse({'error': str(e)}, status=500)
