c9fa0c4b10231c75828c2280c3c6f5797822b448
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-10 17:58:23 +0100

    refactor: Fix typos and update text in components

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ export default function Testimonials() {
                     <h1 className="text-xl font-bold mb-2 ml-16">OUR CUSTOMER STORIES</h1>
                     <h1 className="text-xl text-gray-500 font-semibold mb-10 ml-16">CHECK OUT OUR CASE STUDIES HERE</h1>
                     <div className="chat chat-end">
-                        <div className="chat-bubble bg-[#EEEEEE] text-gray-600 rounded-none p-16">EasyShip has been a fantastic partner for our brand. As a small startup, we had numerous areas we have to cover, many which were outside our wheelhouse. We found Easyship and they helped us incorporate our fulfillment and order processing flow with ease. They offer great customer service and support, going above and beyond to help us get up to speed and ensure our logistics were covered. Thanks EasyShip!</div>
+                        <div className="chat-bubble bg-[#EEEEEE] text-gray-600 rounded-none p-16">EasyShip has been a fantastic partner for our brand. As a small startup, we had numerous areas we have to cover, many which were outside our wheelhouse. We found EasyShip and they helped us incorporate our fulfillment and order processing flow with ease. They offer great customer service and support, going above and beyond to help us get up to speed and ensure our logistics were covered. Thanks EasyShip!</div>
                         <div className="chat-footer">
                             <p className="text-xl font-bold">John Bentley</p>
                             <p className="text-xl font-light">Managing Director</p>
