c20a4827eb9dcc87dfa93f315a03e6a4d7d0c1ce
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-06-11 00:56:54 +0100

    feat: Add Contact page to the application

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Upload from './pages/UploadDoc';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/upload" element={<Upload />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<h1>WRONG PAGE!</h1>} />
         </Routes>
       </div>
